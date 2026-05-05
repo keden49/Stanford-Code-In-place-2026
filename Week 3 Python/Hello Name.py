9abf09ef8d724db2fb446bc92ae6af86316a3029
Kevin Jayden <motukajayden@gmail.com>
2026-05-05 18:09:46 +0300

    Add Hello Name Folder

---
+++ Week 3 Python/Hello Name.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    user_name = input("What is your name?") #prompts user for  name 
+    print(f"Hello {user_name}")
+
+if __name__ == '__main__':
+    main()
