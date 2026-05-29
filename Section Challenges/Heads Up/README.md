f08ca884155bab6e625410eeb707f92b8ae9569d
Kevin Jayden <motukajayden@gmail.com>
2026-05-29 22:38:51 +0300

    Add Heads Up project

---
+++ Section Challenges/Heads Up/README.md
@@ -0,0 +1,21 @@
+# Assignment
+
+Problem #3: Heads Up
+
+Our next goal is to learn how to read data from files. Loading data from a file can be useful for many final projects. Write a program that runs a console version of the phone game Heads Up!
+
+![alt text](image.png)
+
+
+How the game is played remotely:
+
+When it is your turn, close your eyes.
+A word will be displayed in the HeadsUp program.
+The rest of the section will try and describe it without saying the word.
+You have to guess the word as quickly as possible.
+
+Milestones
+
+- Milestone #1: Read the provided function that loads all of the words from the file cswords.txt into a list.
+- Milestone #2: Then, show a randomly chosen word from the list
+- Milestone #3: Repeat: wait for the user to hit enter, then show another word.
---
+++ Section Challenges/Heads Up/headsup.py
@@ -0,0 +1,38 @@
+import random
+
+# Name of the file to read in!
+FILE_NAME = 'cswords.txt'
+
+def get_words_from_file():
+    """
+    This function has been implemented for you. It opens a file, 
+    and stores all of the lines into a list of strings. 
+    It returns a list of all lines in the file. 
+    """
+    lines = []
+    with open(FILE_NAME) as f:
+        for line in f:
+            # removes whitespace characters (\n) from the start and end of the line
+            line = line.strip() 
+            # if the line was only whitespace characters, skip it 
+            if line != "":
+                lines.append(line)
+                
+    return lines
+
+
+def main():
+    words = get_words_from_file()
+
+    while True:
+
+        random_word = random.choice(words)
+        input(random_word)
+        
+
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
